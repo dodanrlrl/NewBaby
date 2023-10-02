64deca1e91be07c2a5648d05c0002b3d913ea357
LeeHaTaeg <gkxor6457@gmail.com>
2023-10-02 13:58:27 +0900

    사운드매니저 리셋 + 사운드 소스

--- Assets/Scripts/LHT/SoundSource.cs
+++ Assets/Scripts/LHT/SoundSource.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum soundType
+{
+    bgm,
+    effect
+}
 public class SoundSource : MonoBehaviour
 {
     private AudioSource _audioSource;
+    [SerializeField] private soundType _soundType;
+    [SerializeField] private AudioClip _audioClip;
 
     public void Play(AudioClip clip, float soundEffectVolume, float soundEffectPitchVariance)
     {

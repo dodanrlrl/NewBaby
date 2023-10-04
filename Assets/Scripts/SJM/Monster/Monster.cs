f21ecacc3e94abfa7a07a696ddcaa3d71156a146
Tealss <aitan@naver.com>
2023-10-04 14:24:54 +0900

    문 수정
    문수정

--- Assets/Scripts/SJM/Monster/Monster.cs
+++ Assets/Scripts/SJM/Monster/Monster.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem.XR;
 
+
+
 public class Monster : MonoBehaviour
 {
 
-
+    // public Gameobject select;
+    public Select sel;
     public float HP;
     public static float speed;
     public static float AD;
@@ -15,10 +18,13 @@ public class Monster : MonoBehaviour
 
     void Start()
     {
-        HP = 100;
+        HP = 1;
         rigid = GetComponent<Rigidbody2D>();
         speed = 3;
         AD = 10.0f;
+        Debug.Log(GameObject.Find("Door1"));
+        sel = FindObjectOfType<Select>();
+        // select = GetComponent<Gameobject>().Nex;
     }
 
     void Update()
@@ -26,8 +32,17 @@ public class Monster : MonoBehaviour
         // 몬스터의 체력이 0 이하로 떨어지면 파괴
         if (HP <= 0)
         {
+            Select.Dies = Select.Dies += 1;
             Destroy(gameObject);
         }
+        Debug.Log(Select.Dies);
+        if (Select.Dies >= 5)
+        {
+            sel.NextStage();
+            Select.Dies = 0;
+
+        }
+
     }
 
     // 다른 스크립트에서 호출하여 몬스터의 체력을 감소시키는 메서드

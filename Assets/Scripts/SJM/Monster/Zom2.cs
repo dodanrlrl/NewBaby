d78545f13f8b1d6282418824ef1deaf481aef41c
SeoJaeMin619 <emiya1663@naver.com>
2023-10-04 10:42:41 +0900

    몬스터 추가

---
+++ Assets/Scripts/SJM/Monster/Zom2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zom2 : MonoBehaviour
+{
+    public static float HP;
+    public static float speed;
+    public static float AD;
+    Rigidbody2D rigid;
+
+    void Start()
+    {
+        HP = 50;
+        rigid = GetComponent<Rigidbody2D>();
+        speed = 5;
+        AD = 12.0f;
+    }
+
+    void Update()
+    {
+        // 몬스터의 체력이 0 이하로 떨어지면 파괴
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // 다른 스크립트에서 호출하여 몬스터의 체력을 감소시키는 메서드
+    public void TakeDamage(float damage)
+    {
+        HP -= damage;
+    }
+}
---
+++ Assets/Scripts/SJM/Monster/Zom2Ai.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zom2Ai : MonoBehaviour
+{
+    Transform target; //당연히 타겟은 플레이어 일 것이다. Transform으로 Target의 위치를 받아온다.
+
+    private void Start()
+    {
+        target = Gamemanager.Instance.player.transform;
+    }
+
+    void Update()
+    {
+        float dis = Vector3.Distance(transform.position, target.position); //내위치와 target의 위치 사이의 거리를 구함
+        if (dis <= 10) // 거리가 10칸 안으로 좁혀졌으면 쫒기 시작
+        {
+            Move();
+        }
+        else return;
+    }
+
+    void Move()
+    {
+        Vector2 targetPosition = new Vector2(target.position.x, target.position.y); // 목표 위치를 가져옵니다.
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y); // 현재 위치를 가져옵니다.
+
+        Vector2 direction = (targetPosition - currentPosition).normalized; // 목표까지의 방향을 정규화합니다.
+
+        // 이미지의 방향을 결정하여 좌우 반전을 적용합니다.
+        if (direction.x > 0) // 플레이어가 오른쪽에 있을 때
+        {
+            transform.localScale = new Vector3(1, 1, 1); // 이미지를 원래 방향으로 설정
+        }
+        else if (direction.x < 0) // 플레이어가 왼쪽에 있을 때
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // 이미지를 좌우 반전
+        }
+
+        // 이제 방향에 따라 이동합니다.
+        transform.Translate(direction * Monster.speed * Time.deltaTime);
+
+    }
+}
